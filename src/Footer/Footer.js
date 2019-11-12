6830e4e3b215d94f539eeecd54f3ddcff8fc322b
Eugene Song <yougene42193@gmail.com>
2019-11-12 13:15:21 -0500

    Footer links to github

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends React.Component {
     render() {
         return(
             <section className="footer">
-                <footer className="github-footer">Created by: Andy Jeong & Eugene Song</footer>
+                <footer className="github-footer">Created by: <a href="https://github.com/nd-jeong" rel="noopener noreferrer" target="_blank">Andy Jeong</a> & <a href="https://github.com/yougene42193" rel="noopener noreferrer" target="_blank">Eugene Song</a></footer>
             </section>
         )
     }
