8f16d1afff146ddf4f4ef56e14ac99f9c03acdbd
Eugene Song <yougene42193@gmail.com>
2019-11-12 13:06:30 -0500

    team logos added to teams section

--- src/Teams/Teams.js
+++ src/Teams/Teams.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Teams.css'
+import {ATL, BKN, BOS, CHA, CHI, CLE, DAL, DEN, DET, GSW, HOU, IND, LAC, LAL, MEM, MIA, MIL, MIN, NOP, NYK, OKC, ORL, PHI, PHX, POR, SAC, SAS, TOR, UTA, WAS} from 'react-nba-logos';
 
 export default class Teams extends React.Component {
     render() {
@@ -8,41 +9,41 @@ export default class Teams extends React.Component {
                 <div className="west-teams">
                     <h3>West</h3>
                     <ul class="teams">
-                        <li>Dallas Mavericks</li>
-                        <li>Denver Nuggets</li>
-                        <li>Golden State Warriors</li>
-                        <li>Houston Rockets</li>
-                        <li>Los Angeles Clippers</li>
-                        <li>Los Angeles Lakers</li>
-                        <li>Memphis Grizzlies</li>
-                        <li>Minnesota Timberwolves</li>
-                        <li>New Orleans Pelicans</li>
-                        <li>Oklahoma City Thunder</li>
-                        <li>Phoenix Suns</li>
-                        <li>Portland Trailblazers</li>
-                        <li>Sacramento Kings</li>
-                        <li>San Antonio Spurs</li>
-                        <li>Utah Jazz</li>
+                        <li><DAL size={50} />Dallas Mavericks</li>
+                        <li><DEN size={50} />Denver Nuggets</li>
+                        <li><GSW size={50} />Golden State Warriors</li>
+                        <li><HOU size={50} />Houston Rockets</li>
+                        <li><LAC size={50} />Los Angeles Clippers</li>
+                        <li><LAL size={50} />Los Angeles Lakers</li>
+                        <li><MEM size={50} />Memphis Grizzlies</li>
+                        <li><MIN size={50} />Minnesota Timberwolves</li>
+                        <li><NOP size={50} />New Orleans Pelicans</li>
+                        <li><OKC size={50} />Oklahoma City Thunder</li>
+                        <li><PHX size={50} />Phoenix Suns</li>
+                        <li><POR size={50} />Portland Trailblazers</li>
+                        <li><SAC size={50} />Sacramento Kings</li>
+                        <li><SAS size={50} />San Antonio Spurs</li>
+                        <li><UTA size={50} />Utah Jazz</li>
                     </ul>
                 </div>
                 <div className="east-teams">
                     <h3>East</h3>
                     <ul class="teams">
-                        <li>Atlanta Hawks</li>
-                        <li>Boston Celtics</li>
-                        <li>Brooklyn Nets</li>
-                        <li>Charlotte Hornets</li>
-                        <li>Chicago Bulls</li>
-                        <li>Cleveland Cavaliers</li>
-                        <li>Detroit Pistons</li>
-                        <li>Indiana Pacers</li>
-                        <li>Miami Heats</li>
-                        <li>Milwaukee Bucks</li>
-                        <li>New York Knicks</li>
-                        <li>Orlando Magic</li>
-                        <li>Philidephia 76ers</li>
-                        <li>Toronto Raptors</li>
-                        <li>Washington Wizards</li>
+                        <li><ATL size={50} />Atlanta Hawks</li>
+                        <li><BOS size={50} />Boston Celtics</li>
+                        <li><BKN size={50} />Brooklyn Nets</li>
+                        <li><CHA size={50} />Charlotte Hornets</li>
+                        <li><CHI size={50} />Chicago Bulls</li>
+                        <li><CLE size={50} />Cleveland Cavaliers</li>
+                        <li><DET size={50} />Detroit Pistons</li>
+                        <li><IND size={50} />Indiana Pacers</li>
+                        <li><MIA size={50} />Miami Heats</li>
+                        <li><MIL size={50} />Milwaukee Bucks</li>
+                        <li><NYK size={50} />New York Knicks</li>
+                        <li><ORL size={50} />Orlando Magic</li>
+                        <li><PHI size={50} />Philidephia 76ers</li>
+                        <li><TOR size={50} />Toronto Raptors</li>
+                        <li><WAS size={50} />Washington Wizards</li>
                     </ul>
                 </div>
             </section>
