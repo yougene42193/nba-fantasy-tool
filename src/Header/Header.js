1e70e66b0f8773c370f6aa394bee13ae0815bfca
Eugene Song <yougene42193@gmail.com>
2020-01-05 15:14:02 -0500

    nav menu changes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 export default class Header extends React.Component {
     render() {
@@ -9,12 +10,15 @@ export default class Header extends React.Component {
                 <nav role='navigation'>
                     <h1>NBA Fantasy Tool</h1>
                     <div className="menu-nav">
-                        <a href="home"><Link to='/'>Home </Link></a>
-                        <a href="schedule"><Link to='/schedule'>Schedule </Link></a>
-                        <a href="teams"><Link to='/teams'>Teams </Link></a>
-                        <a href="standings"><Link to='/standings'>Standing </Link></a>
-                        <a href="reports"><Link to='/reports'>Reports </Link></a>
+                        <ul>
+                            <li className="nav-li"><a href="home"><Link to='/'>Home </Link></a></li>
+                            <li className="nav-li"><a href="schedule"><Link to='/schedule'>Schedule </Link></a></li>
+                            <li className="nav-li"><a href="teams"><Link to='/teams'>Teams </Link></a></li>
+                            <li className="nav-li"><a href="standings"><Link to='/standings'>Standing </Link></a></li>
+                            <li className="nav-li"><a href="reports"><Link to='/reports'>Reports </Link></a></li>
+                        </ul>  
                     </div>
+                    <div className="arrow-down-icon"><IoIosArrowDropdownCircle size="24" /></div>
                 </nav>
             </section>
         )
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -8,71 +8,71 @@ export default class Home extends React.Component {
                 <div className="week-schedule">
                     <div className="4-game week">
                         <h3>4 Games</h3>
-                        <li>NOP</li>
-			            <li>NYK</li>
-			            <li>GSW</li>
-			            <li>WAS</li>
+                        <p>NOP</p>
+			            <p>NYK</p>
+			            <p>GSW</p>
+			            <p>WAS</p>
                     </div>
                     <div class="3-games week">
 			            <h3>3 Games</h3>
-			            <li>NOP</li>
-			            <li>NYK</li>
-			            <li>GSW</li>
-			            <li>WAS</li>
+			            <p>NOP</p>
+			            <p>NYK</p>
+			            <p>GSW</p>
+			            <p>WAS</p>
 		            </div>
                     <div class="2-games week">
 			            <h3>2 Games</h3>
-			            <li>NOP</li>
-			            <li>NYK</li>
-			            <li>GSW</li>
-			            <li>WAS</li>
+			            <p>NOP</p>
+			            <p>NYK</p>
+			            <p>GSW</p>
+			            <p>WAS</p>
 		            </div>
                     <div class="1-games week">
 			            <h3>1 Games</h3>
-			            <li>NOP</li>
-			            <li>NYK</li>
-			            <li>GSW</li>
-			            <li>WAS</li>
+			            <p>NOP</p>
+			            <p>NYK</p>
+			            <p>GSW</p>
+			            <p>WAS</p>
 		            </div>
                 </div>
                 <div class="top-performers">
 		            <h3>Top Performers</h3>
 		            <ul>
-			            <li>J. Harden - HOU</li>
-			            <li>J. Harden - HOU</li>
-			            <li>J. Harden - HOU</li>
-			            <li>J. Harden - HOU</li>
-			            <li>J. Harden - HOU</li>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
 		            </ul>
 	            </div>
                 <div class="most-adds">
 		            <h3>Most Adds</h3>
 		            <ul>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
+						<p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
 		            </ul>
 	            </div>
                 <div class="most-drops">
 		            <h3>Most Drops</h3>
 		            <ul>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
+						<p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
 		            </ul>
 	            </div>
                 <div class="trade-targets">
 		            <h3>Trade Targets</h3>
 		            <ul>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
-			            <li>J. Hart - NOP</li>
+						<p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
+			            <p>J. Harden - HOU</p>
 		            </ul>
 	            </div>
             </section>
